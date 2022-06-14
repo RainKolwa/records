52d4c61aae50aacc3b80413fa12327b1eb859f32
rainkolwa <rainkolwa@gmail.com>
2022-06-14 12:37:26 +0800

    fix: auth check on edit

--- pages/api/records/[id].js
+++ pages/api/records/[id].js
@@ -1,6 +1,7 @@
 import dbConnect from '@/lib/dbConnect';
 import Record from '@/models/Record';
 import withSession from '@/lib/withSession';
+import authCheck from '@/lib/authCheck';
 
 const handler = async (req, res) => {
   const {
@@ -10,14 +11,16 @@ const handler = async (req, res) => {
 
   await dbConnect();
 
+  const post = await Record.findById(id);
+  if (!post) {
+    return res.status(400).json({ code: 1, message: 'Record not found' });
+  }
+  await authCheck({ req, userId: post.author });
+
   switch (method) {
     case 'GET':
       try {
-        const record = await Record.findById(id);
-        if (!record) {
-          return res.status(400).json({ code: 1, message: 'Record not found' });
-        }
-        res.status(200).json({ code: 0, data: record });
+        res.status(200).json({ code: 0, data: post });
       } catch (error) {
         res.status(400).json({ code: 1, message: 'Failed to get record' });
       }
@@ -29,12 +32,9 @@ const handler = async (req, res) => {
           new: true,
           runValidators: true,
         });
-        if (!record) {
-          return res.status(400).json({ code: 1, message: 'Record not found' });
-        }
         res.status(200).json({ code: 0, data: record });
       } catch (error) {
-        res.status(400).json({ code: 1 });
+        res.status(400).json({ code: 1, message: error.message });
       }
       break;
 
