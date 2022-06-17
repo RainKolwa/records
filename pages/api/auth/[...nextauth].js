15254a5be613a0b99b3586142401a6dbe8fa8979
rainkolwa <rainkolwa@gmail.com>
2022-06-17 16:17:35 +0800

    feat: email provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import EmailProvider from 'next-auth/providers/email';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import clientPromise from '@/lib/mongodb';
 import logo from '@/assets/logo.png';
@@ -17,6 +18,10 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
