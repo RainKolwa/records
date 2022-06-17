2cd4dc40131126e1940015078d8d864cdba9aa36
rainkolwa <rainkolwa@gmail.com>
2022-06-17 15:37:48 +0800

    fix: dark chart

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,78 +1,68 @@
-import { ResponsiveCalendar } from '@nivo/calendar';
 import dayjs from 'dayjs';
-import { useEffect, useState } from 'react';
-import useThemeMode from '../hooks/useThemeMode';
+import { ResponsiveCalendar } from '@nivo/calendar';
+import { ThemeContext } from '../pages/_app';
 
 const MyResponsiveCalendar = ({ data, year = dayjs().year() }) => {
   const start = `${year}-01-01`;
   const end = `${year}-12-31`;
-  const colors = {
-    dark: 'red',
-    light: 'gray',
-  };
-  const isDark = useThemeMode();
-  console.log('isDark', isDark);
-  const [emptyColor, setEmptyColor] = useState(
-    isDark ? colors.dark : colors.light
-  );
-  useEffect(() => {
-    if (isDark) {
-      setEmptyColor(colors.dark);
-    } else {
-      setEmptyColor(colors.light);
-    }
-  }, [isDark]);
   return (
-    <ResponsiveCalendar
-      data={data}
-      from={start}
-      to={end}
-      emptyColor={emptyColor}
-      colors={['#f47560', '#61cdbb']}
-      minValue={0}
-      maxValue={100}
-      margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-      yearSpacing={70}
-      yearLegend={() => ''}
-      yearLegendOffset={19}
-      monthBorderWidth={0}
-      monthBorderColor="#ffffff"
-      daySpacing={2}
-      dayBorderWidth={1}
-      dayBorderColor="#ffffff"
-      tooltip={(data) => {
-        if (data.value === undefined) return null;
+    <ThemeContext.Consumer>
+      {({ theme }) => {
+        console.log('theme', theme);
         return (
-          <div
-            style={{
-              color: 'white',
-              backgroundColor: 'black',
-              padding: '10px',
-              fontSize: '14px',
+          <ResponsiveCalendar
+            data={data}
+            from={start}
+            to={end}
+            emptyColor={theme === 'dark' ? '#47474700' : '#eeeeee'}
+            colors={['#f47560', '#61cdbb']}
+            minValue={0}
+            maxValue={100}
+            margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+            yearSpacing={70}
+            yearLegend={() => ''}
+            yearLegendOffset={19}
+            monthBorderWidth={0}
+            monthBorderColor="#ffffff"
+            daySpacing={2}
+            dayBorderWidth={1}
+            dayBorderColor={theme === 'dark' ? 'grey' : '#ffffff'}
+            tooltip={(data) => {
+              if (data.value === undefined) return null;
+              return (
+                <div
+                  style={{
+                    color: 'white',
+                    backgroundColor: 'black',
+                    padding: '10px',
+                    fontSize: '14px',
+                  }}
+                >
+                  <div style={{ fontWeight: 'bold' }}>{data.day}</div>
+                  <ul>
+                    <li>Sleep: {data?.data.sleep} h</li>
+                    <li>Eat: {data?.data.eat} h</li>
+                    <li>Sport: {data?.data.sport} min</li>
+                  </ul>
+                </div>
+              );
             }}
-          >
-            <div style={{ fontWeight: 'bold' }}>{data.day}</div>
-            <ul>
-              <li>Sleep: {data?.data.sleep} h</li>
-              <li>Eat: {data?.data.eat} h</li>
-              <li>Sport: {data?.data.sport} min</li>
-            </ul>
-          </div>
+            legends={[
+              {
+                anchor: 'bottom-right',
+                direction: 'row',
+                translateY: 36,
+                itemCount: 4,
+                itemWidth: 42,
+                itemHeight: 36,
+                itemsSpacing: 14,
+                itemDirection: 'right-to-left',
+              },
+            ]}
+          />
         );
       }}
-      legends={[
-        {
-          anchor: 'bottom-right',
-          direction: 'row',
-          translateY: 36,
-          itemCount: 4,
-          itemWidth: 42,
-          itemHeight: 36,
-          itemsSpacing: 14,
-          itemDirection: 'right-to-left',
-        },
-      ]}
-    />
+    </ThemeContext.Consumer>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,47 @@
 import 'react-toastify/dist/ReactToastify.css';
 import '../css/globals.css';
 import '../css/toast.css';
+import { useState, createContext } from 'react';
 import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
 import { ToastContainer } from 'react-toastify';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 
+export const ThemeContext = createContext({
+  theme: '',
+  setTheme: () => {},
+});
+
 function App({ Component, pageProps: { session, ...pageProps } }) {
+  const [theme, updateTheme] = useState('light');
   return (
-    <SessionProvider session={session}>
-      <Head>
-        <title>Ming</title>
-      </Head>
-      <div className="bg-white dark:bg-slate-800">
-        <div className="container mx-auto">
-          <div className="flex flex-col justify-between min-h-screen">
-            <NavBar />
-            <div className="page-content mb-auto">
-              <Component {...pageProps} />
+    <ThemeContext.Provider
+      value={{
+        theme,
+        setTheme: (mode) => {
+          return mode === 'dark' ? updateTheme('light') : updateTheme('dark');
+        },
+      }}
+    >
+      <SessionProvider session={session}>
+        <Head>
+          <title>Ming</title>
+        </Head>
+        <div className="bg-white dark:bg-slate-800">
+          <div className="container mx-auto">
+            <div className="flex flex-col justify-between min-h-screen">
+              <NavBar />
+              <div className="page-content mb-auto">
+                <Component {...pageProps} />
+              </div>
+              <Footer />
             </div>
-            <Footer />
           </div>
+          <ToastContainer className="my-toast" />
         </div>
-        <ToastContainer className="my-toast" />
-      </div>
-    </SessionProvider>
+      </SessionProvider>
+    </ThemeContext.Provider>
   );
 }
 
